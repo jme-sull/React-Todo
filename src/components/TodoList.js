2b3044f212dcc669989148188ce216d27c794357
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-01 18:50:50 -0400

    inital set up complete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+
+const TodoList = props => {
+
+    return (
+        <div>
+            {props.items.map(item => {
+                <Todo item={item}/>
+            })}
+        </div>
+    )
+}
+
+export default TodoList
